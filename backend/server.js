8d14036c00263cc4bf623d77588510d572b0af33
Alex Stepanov <stepanov.hw@gmail.com>
2023-09-01 12:06:25 -0600

    Configure the locations router

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@
 require("dotenv").config();
 
 // start the mongoose db connection
-require('./config/db.connection.js')
+require("./config/db.connection.js");
 
 // pull PORT from .env, give default value of 4000 and establish DB Connection
 const { PORT } = process.env;
@@ -17,16 +17,32 @@ const express = require("express");
 // create application object
 const app = express();
 
+// import people router
+const locationsRouter = require("./routes/locations");
+
+const cors = require("cors");
+const morgan = require("morgan");
+///////////////////////////////
+// MIDDLEWARE
+////////////////////////////////
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // parse json bodies - this will run before our request accesses the people router
+
+app.use(cors()); // to minimize cors errors, open access to all origins
+app.use(morgan("dev")); // logging for development
+
+// all requests for endpoints that begin with '/people'
+app.use("/locations", locationsRouter);
+
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
 // create a test route
 app.get("/", (req, res) => {
-    res.send("hello world");
+  res.send("hello world");
 });
 
 ///////////////////////////////
 // LISTENER
 ////////////////////////////////
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
-
