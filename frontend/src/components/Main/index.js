77bcadd3e2a88f75782dd44bfba33fb8c6d786ff
Alex Stepanov <stepanov.hw@gmail.com>
2023-09-02 23:54:46 -0600

    Add page _404

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Locations from "../../pages/Locations/";
+import _404 from '../../pages/_404'
 
 const Main = () => {
   return (
-    <main>
+    <main className="flex-grow bg-white">
       <Routes>
         <Route path="/locations" element={<Locations />} />
+        <Route path="/*" element={<_404/>}/>
       </Routes>
     </main>
   );
