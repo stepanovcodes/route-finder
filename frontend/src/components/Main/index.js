feaf2a483357c55ab266acec4aacb8258c24454c
Alex Stepanov <stepanov.hw@gmail.com>
2023-09-05 00:45:47 -0600

    Add Capacities to settings

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Locations from "../../pages/Locations/";
-import _404 from '../../pages/_404'
+import Capacities from "../../pages/Capacities/";
+import NotFound404 from '../../pages/NotFound404'
 
 const Main = () => {
   return (
@@ -8,7 +9,8 @@ const Main = () => {
     <main className="flex-grow bg-white flex flex-col max-w-7xl items-center p-6 lg:px-8 mx-auto">
       <Routes>
         <Route path="/locations" element={<Locations />} />
-        <Route path="/*" element={<_404/>}/>
+        <Route path="/settings" element={<Capacities />} />
+        <Route path="/*" element={<NotFound404/>}/>
       </Routes>
     </main>
   );
---
+++ frontend/src/pages/Capacities/index.js
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react";
+import { Spinner } from "@fluentui/react-components";
+import {
+  getCapacities,
+  createCapacity,
+  deleteCapacity,
+  updateCapacity,
+} from "../../utilities/capacities-service";
+import List from "../../components/List";
+
+const Capacities = (props) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [capacities, setCapacities] = useState([]);
+  const title=`capacities`;
+  const headers=["name","volume","weight","boxes"];
+
+  async function handleGet() {
+    try {
+      const locationsData = await getCapacities();
+      if (locationsData.length) {
+        setCapacities(locationsData);
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+        throw Error(locationsData);
+      }
+    } catch (err) {
+      console.log({ err: err.message });
+    }
+  }
+
+  async function handleCreate(newRow) {
+    try {
+      await createCapacity({
+        name: newRow.name,
+        coordinates: [newRow.longitude, newRow.latitude],
+      });
+      handleGet();
+    } catch (err) {
+      console.log({ err: err.message });
+    }
+  }
+
+  async function handleDelete(id) {
+    try {
+      await deleteCapacity(id);
+      handleGet();
+    } catch (err) {
+      console.log({ err: err.message });
+    }
+  }
+
+  async function handleUpdate(id, editedRow) {
+    try {
+      await updateCapacity(id, {
+        name: editedRow.name,
+        coordinates: [editedRow.longitude, editedRow.latitude],
+      });
+      handleGet();
+    } catch (err) {
+      console.log({ err: err.message });
+    }
+  }
+
+  useEffect(() => {
+    handleGet();
+  }, []);
+
+  const loaded = () => {
+    return (
+      <>
+        <h1 className="text-2xl font-bold tracking-tight">{title.toUpperCase()}</h1>
+        <List
+          inputArray={capacities}
+          handleCreate={handleCreate}
+          handleDelete={handleDelete}
+          handleUpdate={handleUpdate}
+          headers={headers}
+        />
+      </>
+    );
+  };
+
+  const loading = () => (
+    <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <Spinner
+        labelPosition="below"
+        label={`Getting ${title} ready...`}
+      />
+    </div>
+  );
+
+  return isLoading ? loading() : loaded();
+};
+
+export default Capacities;
